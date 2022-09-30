202a44d5e57171112d7d39f659d3fc6e2d143ee7
Uvacoder <uva22940coder@gmail.com>
2022-09-29 22:05:28 -0400

    Update search.vue

--- pages/search.vue
+++ pages/search.vue
@@ -66,7 +66,7 @@ export default {
     async createCopyUrl(resource) {
       try {
         const { path } = resource
-        await this.$copyText(`https://webgems.io${path}`)
+        await this.$copyText(`https://abc-webgems-resources.vercel.app${path}`)
       } catch (e) {
         console.error(e)
       }
