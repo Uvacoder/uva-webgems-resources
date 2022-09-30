02651a1ba54e740e1278416fdf18adc00266164c
Uvacoder <uva22940coder@gmail.com>
2022-09-29 22:02:30 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -33,7 +33,7 @@ export default {
   },
   data() {
     return {
-      prodBaseUrl: 'https://webgems.io/',
+      prodBaseUrl: 'https://abc-webgems-resources.vercel.app/',
       showNotice: false,
     }
   },
