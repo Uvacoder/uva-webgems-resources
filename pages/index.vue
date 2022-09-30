cba2e588c387990372e5069b6c155f6e8c4a167d
Uvacoder <uva22940coder@gmail.com>
2022-09-29 22:04:42 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 .welcome
   main
     h1.text-black.font-bold.text-xl.mb-2 What is&nbsp;
-      span 💎 webgems?
+      span 💎 abc webgems resources?
     p webgems is a goto place for devs and designers to find new resources and more. As a beginner, it's sometimes not easy to find what you need since you don't know where to look for what.
     p Therefore we created this project to have the most common and best resources for beginners and veterans in one place — kind of like a bookmarks list for devs and designers, containing only the best gems out there.
     p If you want to add your gem, go ahead and open a pull request on GitHub (click on that octocat at the top right corner).
@@ -10,9 +10,9 @@
     
     p.muted // made with love by
     p.contributors class&nbsp;
-      a(href="https://twitter.com/lostdesign") @lostdesign&nbsp;
+      a(href="https://twitter.com/VaLandscapes") @lostdesign&nbsp;
       span extends&nbsp;
-      a(href="https://github.com/S3B4S") @S3B4S&nbsp;
+      a(href="https://github.com/uvacoder") @S3B4S&nbsp;
       span and&nbsp;
       a(href="https://devcord.com") devcord
 
